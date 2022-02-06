a921d36a87ba4f7a4987d62b5f5bc8412b7312bc
Rasbats <miker@members.swis.net>
2022-02-06 20:40:03 +0000

    Update Plugin.cmake

--- Plugin.cmake
+++ Plugin.cmake
@@ -86,7 +86,7 @@ SET(SRC
 	)
 
 
-set(PKG_API_LIB api-16)  #  A directory in libs/ e. g., api-17 or api-16
+set(PKG_API_LIB api-17)  #  A directory in libs/ e. g., api-17 or api-16
 
 macro(late_init)
   # Perform initialization after the PACKAGE_NAME library, compilers
